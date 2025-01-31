25fd361d45a6bae5814fb0f9c7bfbdcf725d727b
GeetanshMalik <geetanshmalik337@gmail.com>
2025-02-01 00:23:14 +0530

    Create quotes.js

---
+++ src/data/quotes.js
@@ -0,0 +1,7 @@
+export const quotesDatabase = [
+  { id: 1, text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+  { id: 2, text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+  { id: 3, text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
+  { id: 4, text: "Success is not final, failure is not fatal: it is the courage to continue that counts.", author: "Winston Churchill" },
+  { id: 5, text: "The best way to predict the future is to create it.", author: "Peter Drucker" }
+];
